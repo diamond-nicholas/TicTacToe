de5a59a62cb66a4227950825a198f21b854238f2
marysol <marilirulita@gmail.com>
2021-02-01 09:54:38 -0800

    Add display_board method

--- bin/main.rb
+++ bin/main.rb
@@ -1,25 +1,39 @@
 #!/usr/bin/env ruby
 # class Tictactoe
-  
-  
+
+
   def display_board(board)
-    puts "#{board[0]} | #{board[1]} | #{board[2]}"
-    puts "----------"
-    puts "#{board[3]} | #{board[4]} | #{board[5]}"
-    puts "----------"
-    puts "#{board[6]} | #{board[7]} | #{board[8]}"
+    while board.include?(" ")
+      puts "#{board[0]} | #{board[1]} | #{board[2]}"
+      puts "----------"
+      puts "#{board[3]} | #{board[4]} | #{board[5]}"
+      puts "----------"
+      puts "#{board[6]} | #{board[7]} | #{board[8]}"
+
+      puts "Select a number between 1 to 9, for choose an space"
+      selected = (gets.chomp.to_i) - 1
+      range_arr = (0...9).to_a
+
+      if range_arr.include?(selected)
+        puts "Nice, now is my turn"
+      else
+        puts "Sorry, thats an invalid option, Try again"
+      end
+    end
+  end
+
+  def win_combination
+    winer_combination = true
+    if winer_combination = true
+      puts "Congratulations! you win!"
+    else
+      puts "Sorry , good luck for next game"
+    end
   end
 
   board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
-  
-  
+
   display_board(board)
-  
-  
-  
-  def user_input
-    position = puts "choose a position from 1-9: "
-    position = (gets.chomp().to_i) - 1
-  end
-  
+
+
 # end
