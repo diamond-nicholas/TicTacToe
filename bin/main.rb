13eae7d6ef13e19d5565f48dd664a1980eb368a0
diamond-nicholas <diamondnicholas154@gmail.com>
2021-02-01 18:28:59 +0100

    add the game board and user_input

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,25 @@
 #!/usr/bin/env ruby
+# class Tictactoe
+  
+  
+  def display_board(board)
+    puts "#{board[0]} | #{board[1]} | #{board[2]}"
+    puts "----------"
+    puts "#{board[3]} | #{board[4]} | #{board[5]}"
+    puts "----------"
+    puts "#{board[6]} | #{board[7]} | #{board[8]}"
+  end
 
-puts '"Hello world!"'
+  board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
+  
+  
+  display_board(board)
+  
+  
+  
+  def user_input
+    position = puts "choose a position from 1-9: "
+    position = (gets.chomp().to_i) - 1
+  end
+  
+# end
