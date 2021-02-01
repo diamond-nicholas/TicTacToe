af53a3d3ff18caf69db2bf1e1ae0f51640fdcba4
diamond-nicholas <diamondnicholas154@gmail.com>
2021-02-01 18:57:37 +0100

    fix the linters error

--- bin/main.rb
+++ bin/main.rb
@@ -1,39 +1,28 @@
 #!/usr/bin/env ruby
 # class Tictactoe
 
+def display_board(board)
+  while board.include?(' ')
+    puts "#{board[0]} | #{board[1]} | #{board[2]}"
+    puts '----------'
+    puts "#{board[3]} | #{board[4]} | #{board[5]}"
+    puts '----------'
+    puts "#{board[6]} | #{board[7]} | #{board[8]}"
 
-  def display_board(board)
-    while board.include?(" ")
-      puts "#{board[0]} | #{board[1]} | #{board[2]}"
-      puts "----------"
-      puts "#{board[3]} | #{board[4]} | #{board[5]}"
-      puts "----------"
-      puts "#{board[6]} | #{board[7]} | #{board[8]}"
+    puts 'Select a number between 1 to 9, for choose an space'
+    selected = gets.chomp.to_i - 1
+    range_arr = (0...9).to_a
 
-      puts "Select a number between 1 to 9, for choose an space"
-      selected = (gets.chomp.to_i) - 1
-      range_arr = (0...9).to_a
-
-      if range_arr.include?(selected)
-        puts "Nice, now is my turn"
-      else
-        puts "Sorry, thats an invalid option, Try again"
-      end
-    end
-  end
-
-  def win_combination
-    winer_combination = true
-    if winer_combination = true
-      puts "Congratulations! you win!"
+    if range_arr.include?(selected)
+      puts 'Nice, now is my turn'
     else
-      puts "Sorry , good luck for next game"
+      puts 'Sorry, thats an invalid option, Try again'
     end
   end
+end
 
-  board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
+board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 
-  display_board(board)
-
+display_board(board)
 
 # end
