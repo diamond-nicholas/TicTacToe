e335f1dc66477ae87c1b294c307efec412a23dd5
diamond-nicholas <diamondnicholas154@gmail.com>
2021-02-02 21:50:07 +0100

    add the turn method

--- bin/main.rb
+++ bin/main.rb
@@ -73,52 +73,75 @@ def current_player(turn_count)
   end
 end
 
-def players_name
-  puts 'Enter your name: '
-end
+#turn
+def turn
+  input = puts "Enter a position from 1-9: "
+  input = gets.chomp
+  i = input_to_index(input)
+  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  counter = turn_count(board)
+  current_turn = current_player(counter)
 
-def display_board(board)
-  move = 0
-  while move < 4
-    puts "#{board[0]} | #{board[1]} | #{board[2]}"
-    puts '----------'
-    puts "#{board[3]} | #{board[4]} | #{board[5]}"
-    puts '----------'
-    puts "#{board[6]} | #{board[7]} | #{board[8]}"
-    puts 'now is player 1 turn'
-    puts 'Select a number between 1 to 9, for choose an space'
-    selected = gets.chomp.to_i - 1
-    range_arr = (0...9).to_a
-
-    if range_arr.include?(selected)
-      puts 'Now is player 2 turn'
-    else
-      puts 'Sorry, thats an invalid move, Try again'
-    end
-    move += 1
-  end
-  puts 'Congratulations! you won the game'
-end
-
-# players_turn
-def players_turn
-  puts 'now is player 1 turn'
-end
-# WIN_COMBINATION = []
-
-# win_move or draw_move
-def win_move
-  @win_movement = false
-  @win = false
-  if @win == true
-    puts 'Congratulations! you won the game'
+  if valid_move?(board, i) == true
+    move(board, i, current_turn)
+    display_board(board)
   else
-    puts 'This is a draw game'
+    puts 'Sorry, thats an invalid move, enter another number'
   end
 end
 
-board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 
-display_board(board)
+puts turn
+
+
+
+
+# def players_name
+#   puts 'Enter your name: '
+# end
+
+# def display_board(board)
+#   move = 0
+#   while move < 4
+#     puts "#{board[0]} | #{board[1]} | #{board[2]}"
+#     puts '----------'
+#     puts "#{board[3]} | #{board[4]} | #{board[5]}"
+#     puts '----------'
+#     puts "#{board[6]} | #{board[7]} | #{board[8]}"
+#     puts 'now is player 1 turn'
+#     puts 'Select a number between 1 to 9, for choose an space'
+#     selected = gets.chomp.to_i - 1
+#     range_arr = (0...9).to_a
+
+#     if range_arr.include?(selected)
+#       puts 'Now is player 2 turn'
+#     else
+#       puts 'Sorry, thats an invalid move, Try again'
+#     end
+#     move += 1
+#   end
+#   puts 'Congratulations! you won the game'
+# end
+
+# # players_turn
+# def players_turn
+#   puts 'now is player 1 turn'
+# end
+# # WIN_COMBINATION = []
+
+# # win_move or draw_move
+# def win_move
+#   @win_movement = false
+#   @win = false
+#   if @win == true
+#     puts 'Congratulations! you won the game'
+#   else
+#     puts 'This is a draw game'
+#   end
+# end
+
+# board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+
+# display_board(board)
 
 # end
