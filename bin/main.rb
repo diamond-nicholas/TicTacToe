4e8abc1fdefb85803222fa45197aa880e4423f87
marysol <marilirulita@gmail.com>
2021-02-02 11:37:52 -0800

    Add board display method

--- bin/main.rb
+++ bin/main.rb
@@ -3,6 +3,16 @@
 # class Tictactoe
 
 # enter your name
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
 def players_name
   puts 'Enter your name: '
 end
