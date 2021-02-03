3aacbeb5b45c197459074226c5aecb61c4e1087e
marysol <marilirulita@gmail.com>
2021-02-03 14:29:35 -0800

    Update turn count method

--- bin/main.rb
+++ bin/main.rb
@@ -52,9 +52,7 @@ end
 def turn_count(board)
   counter = 0
   board.each do |element|
-    if element == 'X' || element == 'O'
-      counter += 1
-    end
+    counter += 1 if element.include?('X') || element.include?('O')
   end
   counter
 end
