59ce41c6cfcd7cbddf45331aaf02c7c98a06a0a6
marysol <marilirulita@gmail.com>
2021-02-03 12:57:33 -0800

    Fix linters spaces

--- bin/main.rb
+++ bin/main.rb
@@ -4,25 +4,25 @@
 
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
+  puts '-----------'
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
+  puts '-----------'
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 # WIN_COMBINATION
 WIN_COMBINATION = [
-  [0,1,2], #TOP
-  [3,4,5], #MIDDLE
-  [6,7,8], #BOTTOM
-  [0,3,6], #LEFT
-  [1,4,7], #MIDDLE
-  [2,5,8], #RIGHT
-  [0,4,8], #DIAGONAL LEFT
-  [2,4,6] #DIAGONAL RIGHT
+  [0, 1, 2], # TOP
+  [3, 4, 5], # MIDDLE
+  [6, 7, 8], # BOTTOM
+  [0, 3, 6], # LEFT
+  [1, 4, 7], # MIDDLE
+  [2, 5, 8], # RIGHT
+  [0, 4, 8], # DIAGONAL LEFT
+  [2, 4, 6] # DIAGONAL RIGHT
 ]
 
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 
 # input_to_index
 def input_to_index(user_input)
