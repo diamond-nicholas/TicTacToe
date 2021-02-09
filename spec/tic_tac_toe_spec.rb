cd895f1ba5ba122103717b8d845c9f9f3f33bfb5
diamond-nicholas <diamondnicholas154@gmail.com>
2021-02-09 20:52:01 +0100

    fix rspec for input_to_index method

---
+++ spec/tic_tac_toe_spec.rb
@@ -0,0 +1,19 @@
+require_relative '../lib/play_game'
+
+game = Game.new('X')
+
+
+describe Game do
+  describe '#input_to_index' do
+    it 'converts the user_input to an interger and minus one' do
+      expect(game.input_to_index(7)).to eql(6)
+    end
+  end
+
+  describe '#move' do
+    it 'inserts user_input on the board' do
+      expect(game.move([' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], 4, 'X')).to eql("X")
+    end
+  end
+
+end
