844be558f66c778015ae433ac7bd032a4dc3d165
diamond-nicholas <diamondnicholas154@gmail.com>
2021-02-10 08:22:14 +0100

    test the valid_move? method with rspec

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -17,12 +17,21 @@ describe Game do
   end
 
   describe 'position_taken?' do
-    it 'checks if the postion on the board has a token' do
+    it 'returns false if the postion on the board has no token' do
       expect(game.position_taken?([' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], 1)).to eql(false)
 
     end
   end
 
+  describe 'valid_move?' do
+    it 'returns true if position is not taken' do
+      expect(game.valid_move?([' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], 1)).to eql(true)
+    end
+
+    it 'returns true if the range between zero to nine exclusive include an index on the board' do
+      expect(game.valid_move?([' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], 0...9)).to eql(false)
+    end
+  end
 
 
 end
